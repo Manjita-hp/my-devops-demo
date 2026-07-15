15e5d740f21b19f462dbcc87ce17bdaa2b4300b8
Manjita-hp <manjitamishra90@gmail.com>
2026-07-15 08:46:27 +0545

    Update sample.py

--- lint_demo/sample.py
+++ lint_demo/sample.py
@@ -1,12 +1,8 @@
 def calculate_total(price, quantity):
     discount = 10
     total = price * quantity
-    return total
-    print("done")
 
 def get_status(code):
     if code == 200:
         result = "OK"
     return result
-
-x = undefined_variable + 5
